724a40980afd7e722b9bb0dd8fb0d7e806de9f30
Sogni_Appesi <juegosxepor@gmail.com>
2025-09-07 10:04:55 -0500

    project creation in Spring Boot

---
+++ src/main/java/com/mcsv/McsvLoginApplication.java
@@ -0,0 +1,13 @@
+package com.mcsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsvLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsvLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcsv/McsvLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsvLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
